38e590aa1f6d64a207d9b3b7a5c6e4ee65467a72
SebKoreano <99543845+SebKoreano@users.noreply.github.com>
2024-09-09 00:03:04 -0600

    Clean code

--- TAREA EXTRACLASE 2 - Retos con Listas Enlazadas/Program.cs
+++ TAREA EXTRACLASE 2 - Retos con Listas Enlazadas/Program.cs
@@ -10,44 +10,7 @@ namespace TAREA_EXTRACLASE_2___Retos_con_Listas_Enlazadas
     {
         static void Main(string[] args)
         {
-            DoubleLinkedList lista = new DoubleLinkedList();
 
-            lista.AddLast(1);
-            //lista.AddLast(2);
-            lista.AddLast(3);
-            lista.AddLast(4);
-
-
-            DoubleLinkedList lista2 = new DoubleLinkedList();
-
-            lista2.AddLast(1);
-            //lista2.AddLast(4);
-            lista2.AddLast(2);
-            //lista2.AddLast(6);
-
-            
-
-            lista2.MergeSorted(lista, SortDirection.Asc);
-
-            lista2.InsertInOrder(10);
-
-            //Invert(lista2);
-
-            PrintList(lista2);
-            Console.WriteLine("-------------------------");
-            Console.WriteLine($"Middle: {lista2.GetMiddle()}");
-            Console.WriteLine($"Size: {lista2.size}");
-        }
-
-        static void PrintList(DoubleLinkedList lista)
-        {
-            Node currentNode = lista.head;
-
-            for (int i = 0; i < lista.size; i++)
-            {
-                Console.WriteLine(currentNode.value.ToString());
-                currentNode = currentNode.next;
-            }
         }
 
         public static DoubleLinkedList Invert(DoubleLinkedList lista)
